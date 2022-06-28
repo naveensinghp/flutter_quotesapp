cbf23a6f4ca9689ebbdb9350c8e857fb9dd407e8
NaveenSingh <naveensingh.mtp@gmail.com>
2022-06-29 00:58:00 +0530

    Getting Started into

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 
 
 const lightBlackColor = Color(0xFF2c3130);
+const gilory = 'gilroy';
+// fontFamily: 'gilroy'
---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quotegenerator/constant.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(
+        BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width,
+            maxHeight: MediaQuery.of(context).size.height),
+        context: context,
+        orientation: Orientation.portrait);
+    
+    return  Scaffold(
+      body: Center(child: Text('Spash Screen',
+      style: TextStyle(
+        fontSize: 20.sp,
+        fontFamily: gilory,
+        ),
+      ),),
+    );
+  }
+
+}
